6ad662ad680a441e76c315cde60f97b80d2ce94c
p2jddzmdhr-ui <p2jddzmdhr@privaterelay.appleid.com>
2026-06-22 14:32:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -548,7 +548,7 @@ def detect_category(line: str):
         return 'realme'
     if any(x in line for x in ['Dyson', 'Dreame AirStyle', 'Dreame Aero', 'HT01', 'HD18', 'HS08', 'HS09']):
         return 'dyson'
-    if any(x in line for x in ['Roborock', 'Dreame X', 'Dreame L', 'Dreame H', 'Dreame R', 'Dreame V', 'Dreame G', 'Vacuum']):
+    if any(x in line for x in ['Roborock', 'Dreame X', 'Dreame L', 'Dreame H', 'Dreame R', 'Dreame V', 'Dreame G', 'Vacuum', 'Red Solution', 'Вертикальный', 'Моющий']):
         return 'vacuum'
     if any(x in line for x in ['Insta', 'DJI', 'Osmo']):
         return 'cameras'
